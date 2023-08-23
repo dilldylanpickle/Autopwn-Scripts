27188f1f677e19f39a3c8860f55c1c1b294a38e4
dilldylanpickle <dgonzales4055@gmail.com>
2023-08-23 16:50:14 -0500

    Added more comments to explain stack concepts in source code

--- Find_Offset/vulnerable.c
+++ Find_Offset/vulnerable.c
@@ -8,6 +8,17 @@
  * $ gcc -m32 -fno-stack-protector -z execstack -no-pie vulnerable.c -o vulnerable32
  * $ gcc -fno-stack-protector -z execstack -no-pie vulnerable.c -o vulnerable64
  *
+ * On x86, it should take 44 bytes to reach the instruction pointer
+ *      - Buffer Size: 32 bytes
+ *      - Base Pointer: 4 bytes
+ *      - Instruction pointer 4 bytes
+ *      - Stack Alignment Padding 4 bytes
+ *
+ * On x86_64, it should take 40 bytes to reach the instruction pointer
+ *      - Buffer Size: 32 bytes
+ *      - Base Pointer: 4 bytes
+ *      - Instruction pointer 4 bytes
+ *
  * Note that this code is provided for educational purposes only and should not be used in production environments.
  */
 
